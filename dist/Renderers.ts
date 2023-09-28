8323cf72323b75acbad7e001e4ac7b044c545329
Andreas Göransson <andreas.goransson@endian.se>
2023-09-28 22:14:11 +0200

    Fixed dist for 1.0.17

---
+++ dist/Renderers.ts
@@ -0,0 +1,33 @@
+/**
+ * formhelper
+ *
+ * Copyright (c) Andreas Göransson (https://github.com/agoransson)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { renderControl } from './Control';
+export { renderLabel } from './Label';
+export { renderText } from './Text';
+export { renderPassword } from './Password';
+export { renderEmail } from './Email';
+export { renderSelect } from './Select';
+export { renderTextarea } from './Textarea';
+export { renderFile } from './File';
+export { renderRange } from './Range';
+export { renderCheckbox } from './Checkbox';
+export { renderRadio } from './Radio';
+export { renderDivider } from './Divider';
+export { renderButton } from './Button';
+export { renderSubmit } from './Submit';
+export { renderLink } from './Link';
---
+++ dist/Types.ts
@@ -0,0 +1,33 @@
+/**
+ * formhelper
+ *
+ * Copyright (c) Andreas Göransson (https://github.com/agoransson)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { Control, isControl } from './Control';
+export { LabelControl, isLabel } from './Label';
+export { TextControl, isText } from './Text';
+export { PasswordControl, isPassword } from './Password';
+export { EmailControl, isEmail } from './Email';
+export { SelectControl, isSelect } from './Select';
+export { TextareaControl, isTextarea } from './Textarea';
+export { FileControl, isFile } from './File';
+export { RangeControl, isRange } from './Range';
+export { CheckboxControl, isCheckbox } from './Checkbox';
+export { RadioControl, isRadio } from './Radio';
+export { DividerControl, isDivider } from './Divider';
+export { ButtonControl, isButton } from './Button';
+export { SubmitControl, isSubmit } from './Submit';
+export { LinkControl, isLink } from './Link';
